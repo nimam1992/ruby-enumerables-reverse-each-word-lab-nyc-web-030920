10abe0f7001c08c091e7490bb13ac3b9b5f8453e
Nima Moshtagh <nima.moshtagh1@gmail.com>
2020-02-08 19:45:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
-def practice(x)
+def reverse_each_word(x)
   ors = x.split(" ")
 end
 
-practice('Hello there, and how are you?').collect{|word| p word.reverse}
+reverse_each_word('Hello there, and how are you?').collect{|word| p word.reverse}
